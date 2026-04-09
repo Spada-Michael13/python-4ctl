197940ae42b0e21b252f2e2e942870161f63cb68
Spada-Michael13 <michaelspada104@gmail.com>
2026-04-09 09:41:27 +0200

    Update esercizio_04.py

--- esercizi/esercizio_04.py
+++ esercizi/esercizio_04.py
@@ -4,3 +4,29 @@
 # Realizza una mini applicazione che simuli una calcolatrice.
 # Ogni operazione deve essere gestita tramite una funzione.
 # Se viene inserito il simbolo /, il programma deve uscire.
+def somma(n1,n2):
+  return n1+n2
+def differenza(n1,n2):
+  return n1-n2
+def moltiplicazione(n1,n2):
+  return n1*n2
+def divisione(n1,n2):
+  return n1/n2
+
+operazione=input("inserisci l'operatore(+:somma,-:differenza,*:moltiplicazione,d:divisione,/:per uscire)")
+while operazione !='/':
+  
+  n1=int(input("inserisci il primo numero:"))
+  
+  n2=int(input("inserisci il secondo numero:"))
+  match(operazione):
+    case '+':
+      print(somma(n1,n2))
+    case '-':
+      print(differenza(n1,n2))
+    case '*':
+      print(moltiplicazione(n1,n2))
+    case 'd':
+      print(divisione(n1,n2))
+  operazione=input("inserisci l'operatore(+:somma,-:differenza,*:moltiplicazione,d:divisione,/:per uscire)")
+  print("sei fuori")
