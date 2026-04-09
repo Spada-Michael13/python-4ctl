bba1d0c6afdba39a3b592b1c3eb3978cdc88d730
Spada-Michael13 <michaelspada104@gmail.com>
2026-04-09 08:58:02 +0200

    Update esercizio_01.py

--- esercizi/esercizio_01.py
+++ esercizi/esercizio_01.py
@@ -8,3 +8,11 @@
 # Esempi:
 # - Il numero maggiore è 8
 # - I due numeri sono uguali
+n1=int(input("inserisci il primo numero"))
+n2=int(input("inserisci il secondo numero"))
+if n1>n2:
+  print(f"il valore maggiore è:{n1}")
+elif n1<n2:
+  print(f"il valore maggiore è:{n2}")
+else:
+  print("i valori sono uguali")
