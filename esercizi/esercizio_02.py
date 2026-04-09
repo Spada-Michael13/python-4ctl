c55e0b632b309bc375fe87ddb1d965f9fadfb128
Spada-Michael13 <michaelspada104@gmail.com>
2026-04-09 09:13:27 +0200

    Update esercizio_02.py

--- esercizi/esercizio_02.py
+++ esercizi/esercizio_02.py
@@ -6,3 +6,13 @@
 #
 # calcola e stampa la somma di tutti gli elementi
 # utilizzando sia il costrutto for sia il costrutto while.
+numeri = [4, 7, 2, 9, 1]
+somma_for=0
+for i in numeri:
+  somma_for+=i
+print(f"calcolo con il for:{somma_for}")
+i=0
+somma_while=0
+while i<5:
+  somma_while+=numeri[i]
+print(f"calcolo con il while:{somma_while}")
